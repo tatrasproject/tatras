4f23e190196452a18675c04a003ca38ea76eee1f
GH-CharlesLandau <clandau@guidehouse.com>
2021-04-21 23:52:17 -0400

    kubernetes client connectors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	clients "tatras/clients"
 	h "tatras/handlers"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,9 @@ import (
 // Setup
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+
+	//TODO conditionally create the in-cluster version of the client
+	clients.GetClientSetFromStandalone()
 
 	r.GET("/ping", h.PingHandler)
 
